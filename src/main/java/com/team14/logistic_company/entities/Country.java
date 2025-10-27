2d64e748552521b79d042ac7f956ef90c26c94c7
EliTsekova <80881409+EliTsekova@users.noreply.github.com>
2025-10-27 11:37:17 +0200

    update Country

--- src/main/java/com/team14/logistic_company/entities/Country.java
+++ src/main/java/com/team14/logistic_company/entities/Country.java
@@ -11,7 +11,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.Instant;
 
 @Entity
-@Data
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
